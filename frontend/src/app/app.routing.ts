37c755d4194745fd4fd5548d6b947602f54b7c09
Amit Bhoraniya <amit.bhoraniya@infostretch.com>
2019-03-02 15:56:20 +0530

    removes unused ui

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -81,6 +81,10 @@ export const routes: Routes = [
       {
         path: 'widgets',
         loadChildren: './views/widgets/widgets.module#WidgetsModule'
+      },
+      {
+        path: 'users',
+        loadChildren: './views/users/user.module#UserModule'
       }
     ]
   },
---
+++ frontend/src/app/views/users/user.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { UserComponent } from './user.component';
+import { DashboardRoutingModule } from './user-routing.module';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    DashboardRoutingModule,
+    ChartsModule,
+    BsDropdownModule,
+    ButtonsModule.forRoot()
+  ],
+  declarations: [ UserComponent ]
+})
+export class UserModule { }
